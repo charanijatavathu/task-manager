ed64f3d7d0043057d0acd2ca359decb8dd27aced
vijay120922 <n200305@rguktn.ac.in>
2025-04-01 14:45:35 +0530

    task manager app completed

---
+++ nodejsproject/node-express-course-main/03-task-manager/starter/middleware/async.js
@@ -0,0 +1,11 @@
+const asynwrapper=(fn)=>{
+    return async(req,res,next)=>{
+        try {
+            await fn(req,res,next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports=asynwrapper
