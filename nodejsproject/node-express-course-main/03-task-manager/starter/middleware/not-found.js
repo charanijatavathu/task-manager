04aef63d1196ce9a6b5bdf4941819983cc963240
charanijatavathu <charanijatavathu1001@gmail.com>
2025-04-03 14:14:25 +0530

    Delete nodejsproject/node-express-course-main/03-task-manager directory
    deleted vijays committed files

--- nodejsproject/node-express-course-main/03-task-manager/starter/middleware/not-found.js
+++
@@ -1,4 +0,0 @@
-const notfound=(req,res)=>{
-    res.staus(404).send('route doesnt exist')
-}
-module.exports=notfound
--- nodejsproject/node-express-course-main/03-task-manager/starter/routes/tasks.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const{getAllItems,createTasks,getTask,updateTask,deleteTask}=require('../controllers/tasks')
-router.route('/').get(getAllItems).post(createTasks)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
-
-module.exports=router
